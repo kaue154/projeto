5a6fd13b05b889d0e26cc70a008b36b605311e1b
jcgames2450 <144684890+kaue154@users.noreply.github.com>
2023-09-18 09:05:25 -0300

    atividade

---
+++ ex001.py
@@ -0,0 +1,5 @@
+n1=int( input('digite  oprimeiro numero') ) 
+n2=int( input('digite o segundo numero') )
+r=n1*n2
+print('o resultado e:',r)
+
---
+++ kaue/exercicio.py
@@ -0,0 +1,3 @@
+nome=str(input("digite seu nome"))
+curso=str(input("digite o nome do seu curso"))
+print("ola" ,nome,"seja bem vido ao curso de",curso )
